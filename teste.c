180911bf14dd8363668912b08150d99ecf974bcf
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2020-02-14 10:11:44 -0300

    sei verificar par ou impar

--- teste.c
+++ teste.c
@@ -3,6 +3,18 @@
 
 int main(){
 
+    int num;
+
+    printf("Digite um numero:");
+
+    scanf("%d",&num);
+
+    if(num % 2 == 0){
+        printf("Par...");
+    }else{
+        printf("Impar...");
+    }
+
 
     return 0;
 }
